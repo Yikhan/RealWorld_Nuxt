90d7c0e766e9dc07824adcdd1c775e1844e946e6
Yikhan <yhxiaoch@gmail.com>
2020-08-26 21:32:57 +1000

    实现发布新文章

--- api/article.js
+++ api/article.js
@@ -18,6 +18,23 @@ export const getYourFeedArticles = (params) => {
   })
 }
 
+// 获取文章详情
+export const getArticle = (slug) => {
+  return request({
+    method: 'GET',
+    url: `/api/articles/${slug}/`
+  })
+}
+
+// 发布文章
+export const addArticle = (params) => {
+  return request({
+    method: 'POST',
+    url: `/api/articles`,
+    data: params
+  })
+}
+
 // 添加点赞
 export const addFavorite = (slug) => {
   return request({
@@ -31,14 +48,6 @@ export const deleteFavorite = (slug) => {
   return request({
     method: 'DELETE',
     url: `/api/articles/${slug}/favorite`
-  })
-}
-
-// 获取文章详情
-export const getArticle = (slug) => {
-  return request({
-    method: 'GET',
-    url: `/api/articles/${slug}/`
   })
 }
 
--- pages/editor/index.vue
+++ pages/editor/index.vue
@@ -6,23 +6,38 @@
           <form>
             <fieldset>
               <fieldset class="form-group">
-                <input type="text" class="form-control form-control-lg" placeholder="Article Title" />
+                <input
+                  type="text"
+                  class="form-control form-control-lg"
+                  placeholder="Article Title"
+                  v-model="title"
+                />
               </fieldset>
               <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="What's this article about?" />
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="What's this article about?"
+                  v-model="description"
+                />
               </fieldset>
               <fieldset class="form-group">
                 <textarea
                   class="form-control"
                   rows="8"
                   placeholder="Write your article (in markdown)"
+                  v-model="body"
                 ></textarea>
               </fieldset>
               <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="Enter tags" />
+                <input type="text" class="form-control" placeholder="Enter tags" v-model="tags" />
                 <div class="tag-list"></div>
               </fieldset>
-              <button class="btn btn-lg pull-xs-right btn-primary" type="button">Publish Article</button>
+              <button
+                class="btn btn-lg pull-xs-right btn-primary"
+                type="button"
+                @click="onPostArticle()"
+              >Publish Article</button>
             </fieldset>
           </form>
         </div>
@@ -32,10 +47,39 @@
 </template>
 
 <script>
+import { addArticle } from '@/api/article'
+
 export default {
   // 在路由匹配前先执行中间件
   middleware: 'authenticated',
-  name: "Editor"
+
+  name: 'Editor',
+
+  data() {
+    return {
+      title: '',
+      description: '',
+      body: '',
+      tags: ''
+    }
+  },
+
+  methods: {
+    async onPostArticle() {
+      const params = {
+        article: {
+          title: this.title,
+          description: this.description,
+          body: this.body,
+          tagList: this.tags.split(',')
+        }
+      }
+      const { data } = await addArticle(params)
+      const slug = data.article.slug
+      // 跳转到新发的文章页面
+      this.$router.push(`/article/${slug}`)
+    }
+  }
 }
 </script>
 
